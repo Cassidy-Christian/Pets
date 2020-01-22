6400939b9b22e0b3cc5ea5c0b9c0260f4bec01dd
Allison Jane O Brien <obrienaj@s.dcsdk12.org>
2020-01-22 08:40:43 -0700

    Edit Tester

--- PetsTester.java
+++ PetsTester.java
@@ -1,8 +1,8 @@
 public class PetsTester{
     public static void main(String[] args){
 
-        Cat scout = new Cat(true, true, false);
-        Dog spot = new Dog(true, true, true); 
+        Cat scout = new Cat(true, false, false);
+        Dog spot = new Dog(true, true, false); 
         PetOwner cassidy = new PetOwner(scout, spot);
 
         if (spot.isHappy())
@@ -20,6 +20,22 @@ public class PetsTester{
         else 
             System.out.println("sad boi hours :(");
 
+        scout.petCat();
+        spot.petDog();
 
+        if (spot.isHappy())
+        System.out.println("Woof!");
+        else 
+            System.out.println("woof");
+
+        if (scout.isHappy())
+            System.out.println("meow");
+        else 
+            System.out.println("hiss");
+
+        if (cassidy.isHappy())
+            System.out.println("yeet");
+        else 
+            System.out.println("sad boi hours :(");
     }
 }
