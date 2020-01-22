ae4a594980760cea00d06d3dab55cae0954efd59
Allison Jane O Brien <obrienaj@s.dcsdk12.org>
2020-01-22 08:20:48 -0700

    Edit Cats

--- Cats.java
+++ Cats.java
@@ -3,4 +3,17 @@ public class Cats{
     private boolean fed;
     private boolean walked;
     private boolean petted;
+
+    public Cat(boolean fed, boolean walked, boolean petted){
+        this.fed = fed;
+        this.walked = walked;
+        this.petted = petted;
+    }
+
+    public boolean isHappy(){
+        if (fed && !walked && !petted)
+            return true;
+        else
+            return false;
+    }
 }
--- PetsTester.java
+++ PetsTester.java
@@ -1,7 +1,15 @@
+import com.oracle.webservices.internal.api.EnvelopeStyle;
+
 public class PetsTester{
     public static void main(String[] args){
 
         Cat scout = new Cat(true, true, false)
         Cat sammy = new Cat(true, false, false);
+
+        if (scout.isHappy())
+            System.out.println("meow");
+        else 
+            System.out.println("hiss");
+
     }
 }
